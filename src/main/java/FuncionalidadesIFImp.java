7bec9e2b562b289e37af04df84926061be60207c
Arthur Soares <arthur.soares@ifood.com.br>
2022-11-15 12:02:56 -0300

    fazendo questoes 1 2 e 3

--- src/main/java/FuncionalidadesIFImp.java
+++ src/main/java/FuncionalidadesIFImp.java
@@ -1,20 +1,80 @@
 import models.Raizes;
 import models.Usuario;
 
+import java.util.regex.Pattern;
+
 public class FuncionalidadesIFImp implements FuncionalidadesIF {
+    private BancoDeDados getBancoDeDados() {
+        return bancoDeDados;
+    }
+
+    private void setBancoDeDados(BancoDeDados bancoDeDados) {
+        this.bancoDeDados = bancoDeDados;
+    }
+
+    private BancoDeDados bancoDeDados;
+    private final static Pattern patternEspeciais = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+    private final static Pattern patternNumeros = Pattern.compile("[0-9]");
+    private final static Pattern patternMaiusculos = Pattern.compile("[A-Z]");
+    private final static Pattern patternMinusculos = Pattern.compile("[a-z]");
+    private final static Pattern patternEmail = Pattern.compile("^[a-z0-9.]+@[a-z0-9]+\\..*");
+
+    private boolean validarSenha(String senha) {
+        return senha.length() >= 8 &&
+                patternEspeciais.matcher(senha).find() &&
+                patternNumeros.matcher(senha).results().count() > 2 &&
+                patternMaiusculos.matcher(senha).find() &&
+                patternMinusculos.matcher(senha).find();
+    }
+
+    private boolean validarEmail(String email) {
+        return patternEmail.matcher(email).matches();
+    }
+
     @Override
     public Usuario cadastraUsuario(String nome, String email, String senha) {
-        return null;
+        if (!validarSenha(senha)) {
+            throw new RuntimeException("Senha invalida");
+        }
+        if (!validarEmail(email)) {
+            throw new RuntimeException("Email invalido");
+        }
+
+        if (getBancoDeDados() == null) {
+            try {
+                setBancoDeDados(new BancoDeDados());
+            } catch (Exception e) {
+                System.out.println("Falha ao iniciar conexão com o banco de dados");
+            }
+        }
+
+        Usuario usuario = new Usuario(email, senha, nome);
+        getBancoDeDados().salvaUsuario(usuario);
+        return usuario;
     }
 
     @Override
     public Raizes calculaFuncaoSegundoGrau(double a, double b, double c) {
-        return null;
+        if (a == 0) {
+            throw new RuntimeException("O coeficiente a deve ser diferente de zero!");
+        }
+        double delta = Math.sqrt(Math.pow(b, 2) - (4 * a * c));
+        if (delta == 0) {
+            return new Raizes();
+        } else if (delta > 0) {
+            double x1 = (-b + delta) / 2 * a;
+            double x2 = (-b - delta) / 2 * a;
+            return new Raizes(x1, x2);
+        } else {
+            throw new RuntimeException("A função não tem raizes reais");
+        }
     }
 
     @Override
     public boolean ehTriangulo(double angulo1, double angulo2, double angulo3) {
-        return false;
+        return angulo1 > 0 && angulo2 > 0 && angulo3 > 0 && (
+                (angulo1 + angulo2 + angulo3) == 180
+        );
     }
 
     @Override
@@ -61,4 +121,5 @@ public class FuncionalidadesIFImp implements FuncionalidadesIF {
     public double distanciaEntreDoisPontos(double x1, double y1, double x2, double y2) {
         return 0;
     }
+
 }
--- src/main/java/models/Raizes.java
+++ src/main/java/models/Raizes.java
@@ -1,4 +1,31 @@
 package models;
 
 public class Raizes {
+    private double x1;
+    private double x2;
+
+    public Raizes() {
+
+    }
+
+    public Raizes(double x1, double x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+
+    public double getX1() {
+        return x1;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
 }
