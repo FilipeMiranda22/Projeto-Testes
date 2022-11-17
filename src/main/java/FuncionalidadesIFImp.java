8c6044cd18e31c497b3242e9015d153c32fdf5cc
Filipe Miranda <filipe.salomao22@gmail.com>
2022-11-16 23:57:05 -0300

    Funcionalidades adicionadas

--- src/main/java/FuncionalidadesIFImp.java
+++ src/main/java/FuncionalidadesIFImp.java
@@ -1,6 +1,9 @@
 import models.Raizes;
 import models.Usuario;
 
+import javax.management.InvalidAttributeValueException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.regex.Pattern;
 
 public class FuncionalidadesIFImp implements FuncionalidadesIF {
@@ -91,7 +94,7 @@ public class FuncionalidadesIFImp implements FuncionalidadesIF {
             return false;
         }
         else {
-            throw new RuntimeException("Os lados do triângulo devem ser maiores que 0");
+            throw new RuntimeException("Os lados do triangulo devem ser maiores que 0");
         }
     }
 
@@ -99,51 +102,126 @@ public class FuncionalidadesIFImp implements FuncionalidadesIF {
     public String classificaTriangulo(double angulo1, double angulo2, double angulo3) {
         if (ehTriangulo(angulo1, angulo2, angulo3)){
             if (angulo1 < 90 && angulo2 < 90 && angulo3 < 90){
-                return "Triângulo Acutângulo";
+                return "Triangulo Acutangulo";
             }
             else if (angulo1 == 90 || angulo2 == 90 || angulo3 == 90){
-                return "Triângulo Retângulo";
+                return "Triangulo Retangulo";
             }
             else if (angulo1 > 90 || angulo2 > 90 || angulo3 > 90){
-                return "Triângulo Obtusângulo";
+                return "Triangulo Obtusangulo";
             }
         }
-        return "Os ângulos não correspondem a um triângulo";
+        throw new RuntimeException("Os angulos não correspondem a um triângulo");
     }
 
     @Override
     public boolean ehRetangulo(double angulo1, double angulo2, double angulo3, double angulo4) {
-        return angulo1 == 90 && angulo2 == 90 && angulo3 == 90 && angulo4 == 90;
+        if (angulo1 > 0 && angulo2 > 0 && angulo3 > 0 && angulo4 > 0) {
+            return angulo1 == 90 && angulo2 == 90 && angulo3 == 90 && angulo4 == 90;
+        }
+        else {
+            throw new RuntimeException("Os ângulos devem ser maiores que 0");
+        }
     }
 
     @Override
     public double perimetroRetangulo(double lado1, double lado2) {
-        return 0;
+        if (lado1 > 0 && lado2 > 0) {
+            return lado1 + lado2 + lado1 + lado2;
+        }
+
+        throw new RuntimeException("Os valores que representam os lados do retângulo devem ser positivos!");
     }
 
     @Override
     public double areaRetangulo(double lado1, double lado2) {
-        return 0;
+        if (lado1 > 0 && lado2 > 0) {
+            return BigDecimal.valueOf(lado1 * lado2)
+                    .setScale(2, RoundingMode.HALF_UP)
+                    .doubleValue();
+        }
+        throw new RuntimeException("Os valores que representam os lados do retângulo devem ser positivos!");
     }
 
     @Override
     public double calculadora(String expressao) {
-        return 0;
+        try {
+            String[] operacao = expressao.trim().split("\\s+");
+            if (operacao.length != 3 || operacao[0].isEmpty() || operacao[1].isEmpty() || operacao[2].isEmpty()) {
+                throw new InvalidAttributeValueException();
+            }
+
+            double resultado;
+            double num1 = Double.parseDouble(operacao[0]);
+            double num2 = Double.parseDouble(operacao[2]);
+            String operador = operacao[1];
+
+            switch (operador) {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                    } else {
+                        throw new ArithmeticException();
+                    }
+                    break;
+                case "**":
+                    if (num2 < 0 && num1 == 0 ||  !operacao[2].matches("-?\\d+")) {
+                        throw new ArithmeticException();
+                    }
+                    resultado = Math.pow(num1, num2);
+                    break;
+                default:
+                    throw new ArithmeticException();
+            }
+            return Double.parseDouble(String.format("%.2f", resultado).replace(",", "."));
+        }
+        catch(InvalidAttributeValueException ex){
+            throw  new RuntimeException("Expressão inválida. Sintaxe: \"NUMERO1 OPERADOR NUMERO2\"");
+        }
+        catch(ArithmeticException ex){
+            throw new RuntimeException("\nA operação é inválida. Tente realizar a operação com outros números.\n");
+        }
     }
 
     @Override
     public double areaCirculo(double raio) {
-        return 0;
+        if (raio > 0) {
+            double area = Math.PI * Math.pow(raio, 2);
+            return BigDecimal.valueOf(area)
+                    .setScale(2, RoundingMode.HALF_UP)
+                    .doubleValue();
+        }
+
+        throw new RuntimeException("O valor do raio deve ser positivo!");
     }
 
     @Override
     public double perimetroCirculo(double raio) {
-        return 0;
+        if (raio > 0) {
+            double perimetro = 2 * Math.PI * raio;
+            return BigDecimal.valueOf(perimetro)
+                    .setScale(2, RoundingMode.HALF_UP)
+                    .doubleValue();
+        }
+
+        throw new RuntimeException("\nDados inválidos. Digite um valor numérico e positivo que represente um raio.\n");
     }
 
     @Override
     public double distanciaEntreDoisPontos(double x1, double y1, double x2, double y2) {
-        return 0;
+        double distancia = Math.sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+        return BigDecimal.valueOf(distancia)
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
     }
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,13 @@ public class Main {
         System.out.println(fun.classificaTriangulo(19.4, 126.9, 33.7));
         System.out.println(fun.classificaTriangulo(51.3, 90, 38.7));
         System.out.println(fun.ehRetangulo(90, 90, 90, 90));
+        System.out.println(fun.perimetroRetangulo(5, 5));
+        System.out.println(fun.areaRetangulo(5, 5));
+        System.out.println(fun.areaCirculo(2.72));
+        System.out.println(fun.perimetroCirculo(2.72));
+        System.out.println(fun.distanciaEntreDoisPontos(2, 3, 4, 5));
+
+
+        System.out.println(fun.calculadora("12 ** 2"));
     }
 }
